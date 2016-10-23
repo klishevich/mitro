6ce93f9e8587ee695b5d8b62776b0870487bb4cf
klishevich <busation@gmail.com>
2016-10-23 11:52:58 +0300

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # Change these
-server 'mars.j123.ru', port: 80, roles: [:web, :app, :db], primary: true
+server '139.162.190.203', port: 80, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:klishevich/mitro.git'
 set :application,     'mitro'
