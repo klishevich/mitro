5f604556e31148e83e30bc415770aa3618b91c3c
klishevich <busation@gmail.com>
2016-11-10 00:07:02 +0300

    Add client to Poster works, method PosterIntegration.clients_addClient

--- app/models/poster_integration.rb
+++ app/models/poster_integration.rb
@@ -6,25 +6,42 @@ class PosterIntegration
     uri = URI.parse(url).read
   end
 
-  # def auth
-  # 	Rails.logger.info("start auth")
-  # 	url = 'https://account.joinposter.com/api/auth?client_id=' + 
-  # 	  client_id + '&redirect_uri=' + redirect_uri + '&response_type=code'
-  # 	Rails.logger.info("url #{url}")
-  # 	uri = URI.parse(url).read
-  # 	Rails.logger.info("uri #{uri}")
-  # end
+  def clients_addClient
+    my_url = 'https://busation.joinposter.com/api/clients.addClient?format=json&token=' + token
+    uri = URI(my_url)
+    req = Net::HTTP::Post.new(uri)
+    req.set_form_data("client_name" => "Михаил Клишевич",
+      "client_sex" => 1,
+      "phone" => "9165926645",
+      "card_number" => "123",
+      "client_groups_id_client" => 1,
+      "country" => "Россия",
+      "city" => "Москва",
+      "email" => "busation@gmail.com",
+      "bonus" => 0,
+      "total_payed_sum" => 0,
+      "birthday" => "2016-06-29",
+      "comment" => "test poster add client API")
+    res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+      http.request(req)
+    end
+
+    response_body = JSON.parse(res.body)
+    Rails.logger.info("response_body: #{response_body}")
+
+    # case res
+    # when Net::HTTPSuccess, Net::HTTPRedirection
+    #   # OK
+    # else
+    #   res.value
+    # end
+
+  end
 
   private
-  # def client_id
-  # 	'busation'
-  # end
 
   def token
     return ENV["poster_token"].to_s
   end
 
-  # def redirect_uri
-  # 	'http://localhost:3000/oauth2/callback'
-  # end
 end
