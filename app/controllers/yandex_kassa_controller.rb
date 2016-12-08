8f5b470f9dee2a3f19331ee70c5fa7949e9be5f1
klishevich <busation@gmail.com>
2016-12-08 08:37:51 +0300

    order_check correct xml response

--- app/controllers/yandex_kassa_controller.rb
+++ app/controllers/yandex_kassa_controller.rb
@@ -6,11 +6,19 @@ class YandexKassaController < ApplicationController
   	Rails.logger.info("YandexKassaController order_check begin")
   	shopSumAmount = params[:shopSumAmount]
    	Rails.logger.info("YandexKassaController shopSumAmount #{shopSumAmount}")
-   	data={
-   		:performedDatetime=>'2011-05-04T20:38:01.000+04:00',
-   		:code=>0, 
-   		:invoiceId=>2000000907465,
-   		:shopId=>88596}
-    render xml: data.to_xml(root: "checkOrderResponse")
+    builder = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |xml|
+      xml.checkOrderResponse(
+        'performedDatetime' => '2011-05-04T20:38:01.000+04:00',
+        'code' => '0',
+        'invoiceId' => '2000000907465',
+        'shopId' => '88596')
+    end
+    render xml: builder.to_xml
+   	# data={
+   	# 	:performedDatetime=>'2011-05-04T20:38:01.000+04:00',
+   	# 	:code=>0, 
+   	# 	:invoiceId=>2000000907465,
+   	# 	:shopId=>88596}
+    # render xml: data.to_xml(root: "checkOrderResponse")
   end
 end
